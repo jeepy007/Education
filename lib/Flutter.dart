8d03f3283c8749af3ade3fcfc6b2c8845694119d
iMac-11 <klkjp007@gmail.comgit config --global user.name jeepy007git config user.email klkjp007@gmail.com>
2022-03-30 14:55:41 +0000

    yen

--- lib/Flutter.dart
+++ lib/Flutter.dart
@@ -16,7 +16,7 @@ class _FlutterState extends State<Flutter> {
   void initState() {
     super.initState();
     _controller = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+        'https://www.youtube.com/watch?v=LmtmnKDrmFY&list=PLRNSfzJHcy9FqWhylxQO0ebzcEkpI_ZwO')
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
